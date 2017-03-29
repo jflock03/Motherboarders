f33355d96b89845985f1cffee4865baa116cd231
TeeOhh <Taylorolson50@yahoo.com>
2017-03-29 11:47:08 -0500

    working comparing algorithm

--- syntaxnet_test.py
+++ syntaxnet_test.py
@@ -2,6 +2,8 @@
 import subprocess
 from subprocess import call
 import pickle
+import numpy as np
+import random
 
 class conversation_dictionary(object):
     ## Dictionary object
@@ -77,11 +79,41 @@ class conversation_dictionary(object):
         self.save_dictionary()
         print("Data added successfully.")
         
+    def compare_parsey_vectors(self, vector1, vector2):
+        ##Takes in parseyed vectors1 and 2
+        ##Vectors in the form ((sentence tuple), (word1, POS, senTag), (word2, POS, senTag), ...)
+        tree_vector = vector1[1:]
+        tree_vector2 = vector2[1:]
+        numpy_vector = []
 
-    ## def get_best_response(self, input_vector):
+        #Only compare words up to the length of the shortest sentence
+        for i in range(0, min(len(tree_vector), len(tree_vector2))):
+            for j in range(0, 3):
+                if tree_vector[i][j] == tree_vector2[i][j]:
+                    numpy_vector.append(1)
+                else:
+                    numpy_vector.append(0)
+    
+        numpy_vector = np.array(numpy_vector)
+        return np.linalg.norm(numpy_vector)
+        
+            
+    def get_best_response(self, input_vector):
         ## Takes in an input vector and runs through the dictionary
         ## Finding the closest vector to the input.
         ## Returns a response in string format
+        best_response = ''
+        max_value = 0
+        for key, value in self.dictionary.items():
+            result_score = self.compare_parsey_vectors(key, input_vector)
+            if result_score > max_value:
+                max_value = result_score
+                random_index = random.randint(0, len(value)-1)
+                response_vector = value[random_index][0]
+        
+        return response_vector
+            
+        
 
 def clean_tree(tree):
     ##Input: Takes in the string representing a tree and cleans it
@@ -107,6 +139,8 @@ def clean_tree(tree):
 
 
 def test_run():
+    dic = conversation_dictionary()
+    dic.load_dictionary()
     user_input = input("Text to tag (X to exit): ")
     while user_input != "X":
         #escape single quotes for words such as what're
@@ -115,42 +149,18 @@ def test_run():
             
         stdoutdata = subprocess.check_output('echo ' + user_input + ' | syntaxnet/demo.sh', shell=True)
         stdoutdata = str(stdoutdata,'utf-8')
+        
         vector = clean_tree(stdoutdata)
         print("vector: ", vector)
+        print(dic.get_best_response(vector))
 
         user_input = input("Text to tag (X to exit): ")
-
-
        
 def main():
-    print("Training/Test Menu")
-    print("------------------")
-    print("1. Test parsey on strings")
-    print("2. Create/Load dictionary")
-    print("X. Exit")
-    user_input = input("Choice: ")
-    while user_input != "X":
-        if user_input == "1":
-            #Test run on user strings
-            test_run()
-        elif user_input == "2":
-            #Add data to dictionary
-            dic = conversation_dictionary()
-            data_file = input("File name of text data to add to dictionary: ")
-            dic.add_data(data_file)
-            print_dic = input("Would you like to see the dictionary?(y/n): ")
-            if print_dic == "y":
-                dic.to_string()
-        else:
-            user_input = input("Choose between option 1 or option 2: ")
-        print("\n")
-        print("Training/Test Menu")
-        print("------------------")
-        print("1. Test parsey on strings")
-        print("2. Create/Load dictionary")
-        print("X. Exit")
-        user_input = input("Choice: ")
-   
+##    dic = conversation_dictionary()
+##    dic.add_data('greetings_data.txt')
+    test_run()
+    
   
 main()
 
