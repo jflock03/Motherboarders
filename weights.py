94c8fcf30f9ad2093a93ac2cfe26c6bc8d46eb7f
TeeOhh <Taylorolson50@yahoo.com>
2017-04-09 19:33:40 -0500

    finished get_weights method

--- weights.py
+++ weights.py
@@ -2,20 +2,22 @@ import pickle
 
 class weights(object):
     def __init__(self):
-        self.POS_tags = []
-        self.label_tags = []
+        ## format of dictionary: {[tag]:weight, [tag2]:weight2}
+        self.POS_tags = {}
+        self.label_tags = {}
 
     def get_weight(self, LorP, tag):
         if LorP == "P":
-            #return weight of tag in POS tags list
+            #return weight of tag in POS tags dictionary
+            return self.POS_tags[tag]
         elif LorP = "L":
-            #return weight of tag in label tags list
-    
+            #return weight of tag in label tags dictionary
+            return self.label_tags[tag]
 
     def load_weights(self, POS_fileName, label_fileName):
-        ##Function that loads in the weights list for POS and label tags
+        ##Function that loads in the weights dictionary for POS and label tags
         ##Takes in: the pickled file name for POS weights and label weights
-        ##Returns: Nothing, just stores the weights in the objects lists
+        ##Returns: Nothing, just stores the weights in the objects dictionary
         try:
             self.POS_tags = pickle.load(open("POS_fileName", "rb"))
             self.label_tags = pickel.load(open("label_fileName", "rb"))
